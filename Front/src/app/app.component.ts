72bdf3221fc7e08a0ac143963f99b63599385f44
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-10-10 19:15:22 +0200

    refactor: replace RouterOutlet with PollutionReportComponent in app component

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PollutionReportComponent } from './components/pollution-report/pollution-report.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PollutionReportComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Front';
